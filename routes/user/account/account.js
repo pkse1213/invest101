276ff55d85d3bcc57e7351325e05332023ffeeee
박세은 <pkse1213@naver.com>
2020-01-14 15:44:04 +0900

    [POST] 후원하기

--- routes/user/account/account.js
+++ routes/user/account/account.js
@@ -32,9 +32,7 @@ router.post('/list', auth, function(req, res) {
                 res.json(parseData);
             })        
         }
-    })    
-
-
+    })  
 });
 
 
@@ -83,4 +81,61 @@ router.post('/balance', auth, function(req, res){
     })
 })
 
+router.post('/donation',auth, function(req, res){
+    // console.log(req.decoded);
+    var userId = req.decoded.userId
+    var countnum = Math.floor(Math.random() * 1000000000) + 1;
+    var transId = "T991605500U" + countnum;
+    var finUseNum = req.body.fin_use_num;
+    var bankCode = req.body.bank_code;
+    var accountNum = req.body.account_num;
+    var tranAmt = req.body.tran_amt;
+    var sql = 'SELECT * FROM user WHERE id = ?';
+    dbConnection.query(sql, [userId], function (error, results, fields) {
+        if (error) throw error;
+        var option = {
+            method : "post",
+            url : "https://testapi.openbanking.or.kr/v2.0/transfer/withdraw/fin_num",
+            headers : {
+                Authorization : "Bearer " + results[0].accesstoken
+            },
+            json : {
+                "bank_tran_id" :  transId,
+                "cntr_account_type" :  "N",
+                "cntr_account_num":  "5859040285", // 받는사람 계좌
+                "dps_print_content":"힘내세요",
+                "fintech_use_num":  finUseNum,
+                "wd_print_content":  "오픈뱅킹출금",
+                "tran_amt":tranAmt,
+                "tran_dtime":  "20190910101921", 
+                "req_client_name":"박세은",
+                "req_client_bank_code":bankCode,
+                "req_client_account_num": accountNum,
+                "req_client_fintech_use _num":finUseNum,
+                "req_client_num":"HONGGILDONG1234",
+                "transfer_purpose": "TR",
+                "sub_frnc_name":"하위가맹점",
+                "sub_frnc_num":  "123456789012",
+                "sub_frnc_business_num": "1234567890",
+                "recv_client_name": "INVEST101",
+                "recv_client_bank_code": "035",
+                "recv_client_account_num": "5859040285" // 최종으로 받는사람 계좌
+            }
+        }
+        request(option, function (error, response, body) {
+            // console.log(body);
+            var resultObject = body;
+            if(resultObject.rsp_code == "A0000"){
+                res.json(resultObject);
+            } 
+            else {
+                const data = {
+                    "err_message" : resultObject.rsp_message
+                }
+                res.json(data);
+            }
+    
+        });
+    });
+});
 module.exports = router;
