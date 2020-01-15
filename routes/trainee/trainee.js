0c6fb4eeacac861e7822973330a850e5ef02d606
박세은 <pkse1213@naver.com>
2020-01-15 15:42:34 +0900

    [GET] 아이돌 리스트(후원순)/아이돌 상세

--- routes/trainee/trainee.js
+++ routes/trainee/trainee.js
@@ -4,7 +4,6 @@ var auth = require('../../lib/auth');
 var router = express.Router();
 var connection = require('../../config/dbConfig');
 
-
 router.get('/list', function(req, res){
     var sql = "SELECT * FROM invest101.trainee"
     connection.query(sql, [], function(err, result){
@@ -17,5 +16,40 @@ router.get('/list', function(req, res){
         }
     })
 });
+
+router.get('/:trainee_idx/detail', function(req, res){
+    var traineeId = req.params.trainee_idx;
+    var detail_sql = "SELECT t.*, sum(d.money) as donation_sum FROM invest101.trainee as t JOIN invest101.donation as d ON t.trainee_idx = d.trainee_idx where t.trainee_idx = ?"
+    connection.query(detail_sql, [traineeId], function(err, result){
+        if(err){
+            console.error(err);
+            throw err;
+        }
+        else {
+            var resultObject = result[0];
+            if(resultObject.donation_sum == null) {
+                resultObject.donation_sum = 0;
+            }
+            res.json(resultObject);      
+        }
+    })
+
+});
+
+router.get('/listByDonation', function(req, res){
+    var traineeId = req.params.trainee_idx;
+    var detail_sql = "SELECT sum(d.money) as donation_sum, t.* FROM invest101.donation as d, invest101.trainee as t where t.trainee_idx = d.trainee_idx GROUP BY d.trainee_idx ORDER BY donation_sum DESC";
+    connection.query(detail_sql, [traineeId], function(err, result){
+        if(err){
+            console.error(err);
+            throw err;
+        }
+        else {
+            res.json(result);      
+        }
+    })
+
+});
+
 
 module.exports = router;
